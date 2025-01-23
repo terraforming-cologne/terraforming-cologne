ef926d2f274091a5154d70df65f99ccf34e61626
Jonas Weber <jonas@jole.dev>
2025-01-23 19:00:23 +0100

    More sections

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < ApplicationController
   allow_unauthorized_access
 
   def new
-    redirect_to [:new, :participation] and return if authenticated?
+    redirect_to [:new, :participation], notice: t(".notice") and return if authenticated?
 
     @user = User.new
     session[:return_to_after_authenticating] = new_participation_path
