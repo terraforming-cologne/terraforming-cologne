d37a2790044fce39172b3799e6dce1161fcb6dea
Jonas Weber <jonas@jole.dev>
2025-02-04 09:48:40 +0100

    Order participants by created_at on participants list

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,6 +1,6 @@
 class ParticipantsController < ApplicationController
   def index
     authorize Participant
-    @participants = Participant.active
+    @participants = Participant.active.order(:created_at)
   end
 end
