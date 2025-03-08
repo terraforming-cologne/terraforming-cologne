17debc9c0d87f227a571c12f6f6980a22f0222a0
Jonas Weber <jonas@jole.dev>
2025-03-08 22:15:19 +0100

    Limit number of participants

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
+  MAX_PARTICIPANTS = 100
+
   belongs_to :user
 
   scope :active, -> { joins(:user).merge(User.active) }
@@ -6,6 +8,16 @@ class Participant < ApplicationRecord
   after_create_commit :deliver_confirmation_email_later
   after_update_commit :deliver_paid_email_later, if: :updated_to_paid?
   after_destroy_commit :deliver_cancellation_email_later, if: :paid?
+
+  validate :max_participants
+
+  def self.spots_left
+    MAX_PARTICIPANTS - Participant.count
+  end
+
+  def self.max_reached?
+    Participant.count >= MAX_PARTICIPANTS
+  end
 
   private
 
@@ -23,5 +35,9 @@ class Participant < ApplicationRecord
 
   def deliver_cancellation_email_later
     OrgaMailer.cancellation(user).deliver_now
+  end
+
+  def max_participants
+    errors.add(:base, :max_participants) if Participant.max_reached?
   end
 end
