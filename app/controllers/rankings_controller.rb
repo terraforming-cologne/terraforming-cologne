2b9b92d3eaa619ce8234042ade7463ff8d32fd56
Jonas Weber <jonas@jole.dev>
2025-10-05 19:47:11 +0200

    Add view for rankings

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,9 +2,11 @@ class RankingsController < ApplicationController
   allow_unauthorized_access
 
   def show
-    @tournament = Tournament.find(params[:tournament_id])
-    @round = @tournament.rounds.with_ranking.find_by(number: params[:round] || 1)
-    @ranking_criteria = @round.participations.index_with { |participation| participation.ranking_criteria(@round) }
-    @participations = @round.participations.sort_by { |participation| @ranking_criteria[participation] }.reverse
+    @tournament = Tournament.includes(:rounds).find(params[:tournament_id])
+    @current_round = @tournament.rounds.includes(participations: :user).find_by(number: params[:round] || @tournament.rounds.maximum(:number))
+    @all_rounds = @tournament.rounds
+    @rounds_up_to_current = @tournament.rounds.includes(participations: {scores: :round}).with_ranking.where(number: ..@current_round.number)
+    @ranking_criteria = @rounds_up_to_current.index_with { |round| round.participations.index_with { |participation| participation.ranking_criteria(round) } }
+    @participations = @current_round.participations.sort_by { |participation| @ranking_criteria[@current_round][participation] }.reverse
   end
 end
