6fd1979d5bf1d41e8cdabd72e30ada5a554000bb
Jonas Weber <jonas@jole.dev>
2025-10-06 19:36:55 +0200

    Only consider participations that actually played in ranking controller

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -3,8 +3,9 @@ class RankingsController < ApplicationController
 
   def show
     @tournament = Tournament.find(params[:tournament_id])
+    played_ids = @tournament.participations.includes(:games).filter { |participation| participation.games.count != 0 }.pluck(:id)
     @all_rounds = @tournament.rounds.includes(:participations).merge(Participation.with_ranking_data)
-    @all_participations = @tournament.participations.with_ranking_data.includes(:user, scores: :round)
+    @all_participations = @tournament.participations.with_ranking_data.includes(:user, scores: :round).where(id: played_ids)
     @current_round = @all_rounds.find_by(number: params[:round] || @all_rounds.maximum(:number))
     if @current_round.present?
       @rounds_up_to_current = @all_rounds.where(number: ..@current_round.number)
