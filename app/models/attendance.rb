c4f0a51e91600051ad4e64cec8853944e0cb4369
Jonas Weber <jonas@jole.dev>
2025-10-20 16:26:25 +0200

    Fix ranking view

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,8 +2,6 @@ class Attendance < ApplicationRecord
   include Ranking
 
   belongs_to :participation
-  has_many :seats
-  has_many :games, through: :seats
   has_one :user, through: :participation
   has_one :tournament, through: :participation
 end
