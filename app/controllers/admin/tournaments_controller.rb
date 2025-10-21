66dc7eecfe55d7fe522dd3dd49933c13f8163d64
Jonas Weber <jonas@jole.dev>
2025-10-21 11:26:16 +0200

    68 let players see their current game including their opponents and maybe their preferred language (#86)
    * Start working on tournament overview page
    
    * Split tournament controllers and handle empty state on tournament overview page

---
+++ app/controllers/admin/tournaments_controller.rb
@@ -0,0 +1,48 @@
+class Admin::TournamentsController < ApplicationController
+  before_action :set_tournamen, only: [:edit, :update]
+
+  def index
+    authorize Tournament
+    @next_tournament = Tournament.next
+    @planned_tournaments = Tournament.where(date: (@next_tournament&.date&.next_day || Date.current)..)
+    @previous_tournaments = Tournament.where(date: ..Date.yesterday)
+  end
+
+  def new
+    authorize Tournament
+    @tournament = Tournament.new
+  end
+
+  def create
+    authorize Tournament
+    @tournament = Tournament.new(tournament_params)
+    if @tournament.save
+      redirect_to :tournaments, notice: t(".notice")
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+    authorize @tournament
+  end
+
+  def update
+    authorize @tournament
+    if @tournament.update(tournament_params)
+      redirect_to :tournaments, notice: t(".notice")
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def set_tournament
+    @tournament = Tournament.find(params.expect(:id))
+  end
+
+  def tournament_params
+    params.expect(tournament: [:name, :date, :max_participations])
+  end
+end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,12 +1,5 @@
 class TournamentsController < ApplicationController
-  before_action :set_tournament, only: [:show, :edit, :update]
-
-  def index
-    authorize Tournament
-    @next_tournament = Tournament.next
-    @planned_tournaments = Tournament.where(date: (@next_tournament&.date&.next_day || Date.current)..)
-    @previous_tournaments = Tournament.where(date: ..Date.yesterday)
-  end
+  before_action :set_tournament, only: :show
 
   def show
     authorize @tournament
@@ -20,41 +13,9 @@ class TournamentsController < ApplicationController
     end
   end
 
-  def new
-    authorize Tournament
-    @tournament = Tournament.new
-  end
-
-  def create
-    authorize Tournament
-    @tournament = Tournament.new(tournament_params)
-    if @tournament.save
-      redirect_to :tournaments, notice: t(".notice")
-    else
-      render :new, status: :unprocessable_content
-    end
-  end
-
-  def edit
-    authorize @tournament
-  end
-
-  def update
-    authorize @tournament
-    if @tournament.update(tournament_params)
-      redirect_to :tournaments, notice: t(".notice")
-    else
-      render :edit, status: :unprocessable_content
-    end
-  end
-
   private
 
   def set_tournament
     @tournament = Tournament.find(params.expect(:id))
-  end
-
-  def tournament_params
-    params.expect(tournament: [:name, :date, :max_participations])
   end
 end
