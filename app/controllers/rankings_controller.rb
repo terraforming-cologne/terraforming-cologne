2e7186d062de0fdae11e5efb4499a6241e26cbba
Jonas Weber <jonas@jole.dev>
2025-10-24 13:58:13 +0200

    Fix ranking view

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -5,15 +5,15 @@ class RankingsController < ApplicationController
 
   def show
     @tournament = Tournament.find(params.expect(:tournament_id))
-    played_ids = @tournament.attendances.includes(:games).filter { |attendance| attendance.games.count != 0 }.pluck(:id)
+    played_ids = @tournament.participations.includes(:games).filter { |participation| participation.games.count != 0 }.pluck(:id)
 
     @rounds = @tournament.rounds.where(id: Round.ready_for_ranking)
-    @attendances = @tournament.attendances.with_ranking_data.includes(:user, scores: :round).where(id: played_ids)
+    @participations = @tournament.participations.with_ranking_data.includes(:user, scores: :round).where(id: played_ids)
     if @rounds.any?
       @shown_round = @rounds.find_by(number: params[:round] || @rounds.unscope(:group).maximum(:number))
-      @rounds_up_to_shown = @rounds.includes(:attendances).merge(Attendance.with_ranking_data).where(number: ..@shown_round.number)
-      @ranking_criteria = @rounds_up_to_shown.index_with { |round| @attendances.index_with { |attendance| attendance.ranking_criteria(round) } }
-      @ranked_attendances = @attendances.sort_by { |attendance| @ranking_criteria[@shown_round][attendance] }.reverse
+      @rounds_up_to_shown = @rounds.includes(:participations).merge(Participation.with_ranking_data).where(number: ..@shown_round.number)
+      @ranking_criteria = @rounds_up_to_shown.index_with { |round| @participations.index_with { |participation| participation.ranking_criteria(round) } }
+      @ranked_participations = @participations.sort_by { |participation| @ranking_criteria[@shown_round][participation] }.reverse
     end
   end
 end
