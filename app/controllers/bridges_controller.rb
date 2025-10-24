b0543e7014173484eac94b64f2bd21bd53b49370
Jonas Weber <jonas@jole.dev>
2025-10-24 14:13:20 +0200

    Order rooms and tables in bridge view

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -3,9 +3,9 @@ class BridgesController < ApplicationController
     authorize :bridge
     @tournament = Tournament.find(params.expect(:tournament_id))
     @round = @tournament.rounds.find_by(number: @tournament.current_round_number)
-    @rooms = @tournament.rooms
+    @rooms = @tournament.rooms.order(:number)
     @games = @rooms.index_with do |room|
-      @tournament.games.joins(:round, :room).where(games: {round: @round, tables: {room: room}})
+      @tournament.games.joins(:round, :room).where(games: {round: @round, tables: {room: room}}).order("tables.number")
     end
   end
 end
