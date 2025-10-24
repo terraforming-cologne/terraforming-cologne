9ba38e833707d2b89b561902d683e3fc6de177d3
Jonas Weber <jonas@jole.dev>
2025-10-24 13:07:07 +0200

    Update AddRoundReferenceToAttendances migration to update present data

--- db/migrate/20251023183828_add_round_reference_to_attendances.rb
+++ db/migrate/20251023183828_add_round_reference_to_attendances.rb
@@ -1,5 +1,24 @@
 class AddRoundReferenceToAttendances < ActiveRecord::Migration[8.0]
   def change
-    add_reference :attendances, :round, null: false, foreign_key: true
+    add_reference :attendances, :round, null: true, foreign_key: true
+    reversible do |direction|
+      direction.up do
+        execute <<~SQL
+          UPDATE attendances
+          SET round_id = 1;
+
+          INSERT INTO attendances (participation_id, round_id, created_at, updated_at)
+          SELECT participation_id, 2, created_at, updated_at
+          FROM attendances
+          WHERE round_id = 1;
+
+          INSERT INTO attendances (participation_id, round_id, created_at, updated_at)
+          SELECT participation_id, 3, created_at, updated_at
+          FROM attendances
+          WHERE round_id = 1;
+        SQL
+      end
+    end
+    change_column_null :attendances, :round, true
   end
 end
