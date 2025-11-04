46d910c61f870073317ba32490225806f3a74e53
Jonas Weber <jonas@jole.dev>
2025-11-04 17:32:07 +0100

    Allow all browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   include SpamProtection
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern unless Rails.env.development?
+  # allow_browser versions: :modern
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
