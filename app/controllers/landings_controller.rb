c1298dd5d117fd8fe8b46ae6b4e968a94b8d028f
Jonas Weber <jonas@jole.dev>
2025-11-09 18:08:49 +0100

    Remove landings

--- app/controllers/landings_controller.rb
+++
@@ -1,6 +0,0 @@
-class LandingsController < ApplicationController
-  def show
-    @tournament = Tournament.find(params.expect(:tournament_id))
-    authorize @tournament, policy_class: LandingPolicy
-  end
-end
